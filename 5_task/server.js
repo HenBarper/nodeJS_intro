189306b0db6c9ab35662a07fd1cd4f9224417820
Ben Harper <benjaminitonio@gmail.com>
2024-02-20 15:04:24 -0600

    Work on task 5 - 13:32m

---
+++ 5_task/server.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
+const logEvents = require('./logEvents');
+const EventEmitter = require('events');
+class Emitter extends EventEmitter {};
+// Initialize object
+const myEmitter = new Emitter();
+
+// Port definition
+const PORT = process.nextTick.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+            break;
+    }
+
+    let filePath = 
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+});
+// 13:32m
+
+// This should always be at the end of your server.js file
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// Add listener for the log event
+// myEmitter.on('log', (msg) => logEvents(msg));
+
+// myEmitter.emit('log', 'Log event emitted!');
